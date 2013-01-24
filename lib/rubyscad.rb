cf2fc90b8de4a28a2ef409f527c04ac03196355a
cjbissonnette <cjbissonnette@gmail.com>
2013-01-24 10:47:33 -0500

    fixed file output crash on windows

--- lib/rubyscad.rb
+++ lib/rubyscad.rb
@@ -1,8 +1,2 @@
-# I put this file here to both make bundler happy with a gem named 'rubyscad'
-# (all lowercase) and to provide a place to start breaking the project into
-# smaller components.
-#
-# Hope that wasn't too presumptious :-\
-require 'RubyScad'
-
 require 'rubyscad/version'
+require 'RubyScad'
